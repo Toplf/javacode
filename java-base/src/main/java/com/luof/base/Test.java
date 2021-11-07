40f739cac1c09faca775b7583054c8efcc342a19
luofang <3346338990@qq.com>
2021-11-07 20:15:57 +0800

    java类型和语法

--- java-base/src/main/java/com/luof/base/Test.java
+++ java-base/src/main/java/com/luof/base/Test.java
@@ -4,5 +4,10 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello word");
+
+        // 强制类型转换
+        String number = "34";
+        int a = Integer.parseInt(number);
+        System.out.println(a);
     }
 }
